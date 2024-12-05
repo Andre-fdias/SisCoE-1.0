25dba90136a7fc98a5acf79ac911ff32eeb81470
Andre Dias <andrefonsecadias21@gmail.com>
2024-12-05 17:37:38 -0300

    Implementação do app Core, .env, .gitgnore e implementação dos templates

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    template_name = 'index.html'
+    return render(request, template_name)
+
+
+@login_required
+def dashboard(request):
+    template_name = 'dashboard.html'
+    return render(request, template_name)
